cac741afa5bb37e4d56914041863b687395d67f1
Iyetule <105356802+Iyetule@users.noreply.github.com>
2022-06-22 11:08:40 +0100

    Javascript Introductory page

---
+++ script.js
@@ -0,0 +1,8 @@
+//this is my first javasript code!
+console.log('introdutory page');
+let name = 'Racheal Okoka';
+let height = '5.ft';
+let country = 'nigeria'; 
+console.log(name,height,country);
+
+
